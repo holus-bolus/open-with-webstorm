aa8a41e40be77a627a2f7978b48a489869325838
Oleksii Ustinov <93076661+holus-bolus@users.noreply.github.com>
2023-09-19 12:36:19 +0300

    Create open-with-webstorm.sh

---
+++ open-with-webstorm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Opening with WebStorm..."
+     C:\Program Files\JetBrains\WebStorm 2023.1.3\bin\webstorm64.exe --idea.config.path $HOME/.webstorm2023.2/config
+          C:\Program Files\JetBrains\WebStorm 2023.1.3\bin\webstorm64.exe $GITHUB_WORKSPACE
